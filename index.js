e8604f8477447d47d46c263743602ff77d844f24
Andrew Barnes <andrewjbarnes94@gmail.com>
2022-06-17 22:55:31 -0400

    Version 1 of Node-Stock

--- index.js
+++ index.js
@@ -2,17 +2,62 @@ const express = require('express');
 const app = express();
 const { engine } = require ('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+// Use Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+// API KEY pk_ae4ec04a46e24d5ab079d55ac8d894c9
+// Create Call API Function
+function call_api(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_ae4ec04a46e24d5ab079d55ac8d894c9', { json: true }, (err, res, body) => {
+        if (err) {return console.log(err);}
+        if (res.statusCode === 200){
+            console.log(body)
+            finishedAPI(body)
+        };
+    });
+};
 
 // Set Handlebars Middleware
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
-// Set Handlebars Routes
+// Set Handlebars Index Route
 app.get('/', function (req, res) {
-    res.render('home');
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI
+        });    
+    });
+});
 
+// Set Handlebars index GET Route
+app.get('/', function (req, res) {
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI,
+        });    
+    });
+});
+
+// Set Handlebars Index POST Route
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+        posted_stuff = req.body.tickerSymbol;
+        res.render('home', {
+            stock: doneAPI,
+            posted_stuff: posted_stuff
+        });    
+    }, req.body.tickerSymbol);
+});
+
+// Set Handlebars About Route
+app.get('/about', function (req, res) {
+    res.render('about');
 });
 
 // Set Static Folder
